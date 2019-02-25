978d79d0b3fb7aca28a691374ae47bab0b535ddc
eduar25 <315073766@ciencias.unam.mx>
2019-02-25 10:53:14 -0600

    fdamfda

---
+++ Practica2/Proyecto2Preguntas.py
@@ -0,0 +1,40 @@
+print("Preguntas, Funciones")
+print("Las funciones resultan ventajosas ya que al diseñar basta con introducir la función, y no tienes que escribir nuevamente todo el código para que se realice una determinada acción. Por ejemplo, al generar mi programa de los números primos, al no contar con Juyter aún, se me complicó demasiado en cuestión de espacio")
+print("El cógido de la función se ejecuta cuando llamamos a la función,es en ese momento cuando el código que generamos se corre por primera vez")
+print("El enunciado que crea una función es def funcion ()")
+print("La diferencia entre una función y una llamada de función es que la primera es el código asignado, así como las variables que usa, mientras que la llamada de función es cuando tu citas a la función para que realice la tarea para la que fue programada")
+print("Me parece que pueden haber tantos ambitos locales como funciones tú quieras definir, en el caso de los ambitos globales me parece que sólo hay uno.")
+print("No les pasa nada, se ejecuta la función que has llamado pero a corde con las variables locales que ya has establecido.")
+print("Un valor de retorno es una parte del programa que te permite regresar a una sección en específico de la función, Sí puede")
+print("Pues no hay valor de retorno, simplemente la función seguirá su curso") 
+print("Para forzarlo basta con que indiques que se trata de una variable global, y así no trabajas con una nueva variable local con el mismo nombre que la variable global que querías")
+print("Los tipos de datos None representan un valor vacío, no inexistente, sólo vacío, así como el conjunto que contiene al cero no es igual al conjunto vacio")
+print("La declaración import random va a tomar una función de otra libreria, las cuales por lo general tienen que descargarse ya que la versión de python que tenemos no la poseen") 
+print("Primero debes citar el módulo en el cual está contenida y luego a la función en sí: nombre del modulo. nombre de la función ()")  
+
+print("Listas y Tuplas")
+print("[] indica que se trata de una lista, en este caso sólo se creo, no tiene elementos dentro aún")
+
+print("Supongamos que tenemos una lista llamada b = [1,1,1,1], entonces para insertar el valor hola en esta lista empleamos el comando b.insert(2,Hola), esto es si contemplamos que los valores de la lista se empiezan a numerar desde 0 y queremos que ocupe la tercer posición. En el caso de la variable spam sería lo mismo: spam.insert(2,Hola), si ya hay un valor en esa posición y lo que buscamos es reemplazarlo por el valor Hola, empleamos la asignación spam[2] = Hola ")
+
+print("Debe de marcar un error, ya que el resultado de esa operación es 6/11, y no en la lista spam cada elemento tiene su posición determinada por un número entero, y al intentar buscar el elemento en la posición 6/11 nos daría error")
+print("Al evaluar spam[-1], veremos que cuando asigna una posición a cada elemento de la lista con un entero empieza en 0, de modo que el valor -1 es el último en ser asignado, al pedirle el elemento en esta posición te dará el último elemento, en este caso 10")
+print("Evalua [2,4], puesto que esta función te devuelve los dos primeros elementos")
+
+print("La función index te indica en qué posición de una determinada lista se encuentra un elemento en específico, en este caso vuscamos en la lista bacot el elemento cat, el cual se encuentra 2 veces, pero esta función nos dirá dónde se encuentra el primer elemento que cumple con la condición recorriendo la lista de izquierda a derecha")
+
+print("La función append te añade un nuevo elemento a la lista, al final de esta, por ejemplo, añadirá el elemento 99 al final de la lista")
+
+print("La función remove va a eliminar de la lista al elemento que especifiquemos, como tenemos dos veces ese elemento, eliminará solamente al primero, recordando que la función va de izquierda a derecha")
+
+print("No puedo responder la pregunta de la concatenación de listas y la replicación :c")
+
+print("La diferencia es que append unicamente inserta un elemento a la lista al final de esta, el comando insert te permite además indicar la posición en que quieres insertar el elemento")
+
+print("Las listas son mutables y las tuplas son inmutables")
+
+
+
+
+
+ 
